b0955d30445bdad8ed6f734d39a689275e14aec7
Ivan Zakharov <ibzakharov2@asuproject.ru>
2024-08-11 23:58:17 +0200

    Add migrations

---
+++ src/TreeNodeException.Api/Migrations/20240810210125_RemoveTree.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TreeNodeException.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTree : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Nodes_Trees_TreeID",
+                table: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "Trees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Nodes_TreeID",
+                table: "Nodes");
+
+            migrationBuilder.DropColumn(
+                name: "TreeID",
+                table: "Nodes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TreeID",
+                table: "Nodes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Trees",
+                columns: table => new
+                {
+                    TreeID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TreeName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trees", x => x.TreeID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_TreeID",
+                table: "Nodes",
+                column: "TreeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Nodes_Trees_TreeID",
+                table: "Nodes",
+                column: "TreeID",
+                principalTable: "Trees",
+                principalColumn: "TreeID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ src/TreeNodeException.Api/Migrations/20240811215252_UpdateNode.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TreeNodeException.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Nodes_Nodes_ParentNodeID",
+                table: "Nodes");
+
+            migrationBuilder.RenameColumn(
+                name: "NodeID",
+                table: "Nodes",
+                newName: "NodeId");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentNodeID",
+                table: "Nodes",
+                newName: "ParentId");
+
+            migrationBuilder.RenameColumn(
+                name: "NodeName",
+                table: "Nodes",
+                newName: "Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Nodes_ParentNodeID",
+                table: "Nodes",
+                newName: "IX_Nodes_ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Nodes_Nodes_ParentId",
+                table: "Nodes",
+                column: "ParentId",
+                principalTable: "Nodes",
+                principalColumn: "NodeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Nodes_Nodes_ParentId",
+                table: "Nodes");
+
+            migrationBuilder.RenameColumn(
+                name: "NodeId",
+                table: "Nodes",
+                newName: "NodeID");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentId",
+                table: "Nodes",
+                newName: "ParentNodeID");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Nodes",
+                newName: "NodeName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Nodes_ParentId",
+                table: "Nodes",
+                newName: "IX_Nodes_ParentNodeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Nodes_Nodes_ParentNodeID",
+                table: "Nodes",
+                column: "ParentNodeID",
+                principalTable: "Nodes",
+                principalColumn: "NodeID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
