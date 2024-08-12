745a4622dbddca1332ce6e30db4cc4077731e42d
Ivan Zakharov <ibzakharov2@asuproject.ru>
2024-08-13 00:34:44 +0200

    Clear code

--- src/TreeNodeException.Api/Program.cs
+++ src/TreeNodeException.Api/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using TreeNodeException.Api.Middlewares;
 using TreeNodeException.Api.Middlewares.Extensions;
 using TreeNodeException.Api.Models;
 using TreeNodeException.Api.Repositories;
@@ -12,52 +11,35 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
-        
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql("Host=desktop;Port=8001;Database=testdb;Username=postgres;Password=postgres"));
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Добавление DbContext в сервисы
-        // builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        //     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-        
-        
         builder.Services.AddScoped<INodeRepository, NodeRepository>();
         builder.Services.AddScoped<ITreeRepository, TreeRepository>();
-        builder.Services.AddScoped<IExceptionLogRepository, ExceptionLogRepository>();
-
         builder.Services.AddAutoMapper(typeof(Program));
 
         builder.Services.AddControllers();
         
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(c =>
-        {
-            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            c.IncludeXmlComments(xmlPath);
-        });
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-        
+
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-        
+
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
         app.MapControllers();
-        
-         // app.UseRedirect();
+
         app.UseException();
-        
+
         app.Run();
     }
 }
